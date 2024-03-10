08501ebfdd8679c5282183b6298a0c768a6abd89
yy-beep <ipro6722@gmail.com>
2024-03-10 07:39:02 +0100

    Starting Rock Paper Scissors Project
    making getComputerChoice() and randomNumber() function

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+function randomNumber() {
+    
+}
+
+// create function called randomNumber()
+// make so random number from 1 to 100 appears
+
+function getComputerChoice() {
+
+}
+
+// write a function called getComputerChoice()
+// set up if statement for rock
+// set up if else statement for paper
+// set up else statement for scissors
+// use math to make each of them appear randomly ???

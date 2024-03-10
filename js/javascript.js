fe2fb740d67f54818cb54bfdb2e0f6a14cf4f9d2
yy-beep <ipro6722@gmail.com>
2024-03-10 08:35:38 +0100

    mission failed will get them next time

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,25 @@
 function randomNumber() {
-    const randomNumber = Math.floor(Math.random() * 10 + 1);
-    return randomNumber;
+    return Math.floor(Math.random() * 10 + 1);
 }
 
 console.log(randomNumber())
 
 // create function called randomNumber()
-// make so random number from 1 to 100 appears
+// make so random number from 1 to 10 appears
 
 function getComputerChoice() {
+    let choice = randomNumber()
 
+    if (choice = 1 || 2 || 3) {
+        return "Rock"
+    } else if (choice = 4 || 5 || 6) {
+        return "Paper"
+    } else if (choice = 7 || 8 || 9) {
+        return "Scissors"
+    };
 }
+
+console.log(getComputerChoice())
 
 // write a function called getComputerChoice()
 // set up if statement for rock

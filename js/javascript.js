ff8ecc221831cd06407fe9dec8f116cc39670755
yy-beep <ipro6722@gmail.com>
2024-03-12 17:22:34 +0100

    Failed to make playRound() function work

--- js/javascript.js
+++ js/javascript.js
@@ -2,25 +2,41 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-console.log(randomNumber(1, 3))
-
 // create function called randomNumber()
 // make so random number from 1 to 3 appears
 
 function getComputerChoice() {
-    let choice = randomNumber(1, 3)
+    let computerChoice = randomNumber(1, 3)
 
-    if (choice == 1) {
+    if (computerChoice == 1) {
         return "Rock"
-    } else if (choice == 2) {
+    } else if (computerChoice == 2) {
         return "Paper"
     } else { return "Scissors"};
 }
-
-console.log(getComputerChoice())
 
 // write a function called getComputerChoice()
 // set up if statement for rock
 // set up if else statement for paper
 // set up else statement for scissors
 // use math to make each of them appear randomly ???
+
+function playRound(playerSection, computerSection) {
+    let playerSection = promt("What do you play? ", " ")
+    getComputerChoice()
+
+    if (playerSection == "Rock" || getComputerChoice() == "Scissors") {
+        alert("You won! Rock beats Scissors!!!")
+    } else if (playerSection == "Scissors" || getComputerChoice() == "Paper") {
+        alert("You won! Scissors beat Paper")
+    } else if (playerSection == "Paper" || getComputerChoice() == "Rock") {
+        alert("You won!!! Paper beats Rock!!!")
+    }
+}
+
+// create a function called  playRound(), to play round of rock, paper, scissors
+// playRound() takes 2 parametars 1. playerSection 2. computerSection
+// playerSection gets it's argument via promt
+// computerSection gets it's argument via getComputerChoice() function
+// make it print out a massage like You lose! Paper beats Rock!!! for all scenarios
+// ??? make so playerSection() promt takes in input no matter how written (rock, Rock, RoCk, esc..) ???
